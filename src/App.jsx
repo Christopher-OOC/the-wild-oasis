af59adef454488434eae0fe697e9449d403069c7
Christopher-OOC <olojedechristopher24@gmail.com>
2025-08-30 05:36:37 +0100

    coimmit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -14,10 +14,25 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Button>Check In</Button>
-        <Button>Check Out</Button>
-        <Input as="number" />
+        <Row>
+          <Row type="horizontal">
+            <div>
+              <Heading as="h1">The Wild Oasis</Heading>
+              <Button variation="primary" size="medium">
+                Check In
+              </Button>
+              <Button>Check Out</Button>
+            </div>
+          </Row>
+
+          <Row type="vertical">
+            <div>
+              <Heading as="h2">Check In and Out</Heading>
+              <Input as="number" />
+              <Input as="number" />
+            </div>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
