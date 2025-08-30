4a5de6f97d0d095fef541518f8024a0e507e84ba
Christopher-OOC <olojedechristopher24@gmail.com>
2025-08-30 05:00:09 +0100

    coommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem, 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0%.8rem 1.2rem;
-`;
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -35,10 +14,10 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
         <Button>Check In</Button>
         <Button>Check Out</Button>
-        <Input type="number" />
+        <Input as="number" />
       </StyledApp>
     </>
   );
